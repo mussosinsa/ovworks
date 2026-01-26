2289d133b3d62b1bae0e187d5439085012c819d2
mussosinsa <62009100+mussosinsa@users.noreply.github.com>
2026-01-26 13:40:22 +0900

    Add dummy column to audit log table

--- frontend/webadmin/modules/webadmin/src/main/java/org/ovirt/engine/ui/webadmin/section/main/view/MainAuditLogView.java
+++ frontend/webadmin/modules/webadmin/src/main/java/org/ovirt/engine/ui/webadmin/section/main/view/MainAuditLogView.java
@@ -1,7 +1,10 @@
 package org.ovirt.engine.ui.webadmin.section.main.view;
 
 import org.ovirt.engine.ui.common.uicommon.model.MainModelProvider;
+import org.ovirt.engine.ui.common.widget.table.SimpleActionTable;
+import org.ovirt.engine.ui.common.widget.table.column.AbstractTextColumn;
 import org.ovirt.engine.ui.uicommonweb.models.AuditLogListModel;
+import org.ovirt.engine.ui.webadmin.gin.ClientGinjectorProvider;
 import org.ovirt.engine.ui.webadmin.section.main.presenter.MainAuditLogPresenter;
 import org.ovirt.engine.ui.webadmin.section.main.view.popup.security.AuditLogProtectionTabView;
 import org.ovirt.engine.ui.webadmin.section.main.view.popup.security.AuditLogRemoteBackupTabView;
@@ -21,6 +24,30 @@ public class MainAuditLogView extends AbstractMainWithDetailsTableView<Object, A
 
     private SimplePanel contentPanel;
     private FlowPanel sidebar;
+
+    @Override
+    protected SimpleActionTable<Void, Object> createActionTable() {
+        SimpleActionTable<Void, Object> table = new SimpleActionTable<Void, Object>(getModelProvider(),
+                getTableResources(), ClientGinjectorProvider.getEventBus(),
+                ClientGinjectorProvider.getClientStorage()) {
+            {
+                showRefreshButton();
+                showItemsCount();
+                showSelectionCountTooltip();
+                enableHeaderContextMenu();
+            }
+        };
+
+        // Add a dummy column to prevent rendering errors when table has data but no columns
+        table.addColumn(new AbstractTextColumn<Object>() {
+            @Override
+            public String getValue(Object object) {
+                return ""; //$NON-NLS-1$
+            }
+        }, ""); //$NON-NLS-1$
+
+        return table;
+    }
 
     @Inject
     public MainAuditLogView(MainModelProvider<Object, AuditLogListModel> modelProvider,
