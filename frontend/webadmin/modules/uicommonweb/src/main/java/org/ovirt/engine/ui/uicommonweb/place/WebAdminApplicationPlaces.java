a1a10459d1560ec71d47108f158422a0abc103e3
mussosinsa <62009100+mussosinsa@users.noreply.github.com>
2026-01-26 16:58:49 +0900

    Remove audit log management views

--- frontend/webadmin/modules/uicommonweb/src/main/java/org/ovirt/engine/ui/uicommonweb/place/WebAdminApplicationPlaces.java
+++ frontend/webadmin/modules/uicommonweb/src/main/java/org/ovirt/engine/ui/uicommonweb/place/WebAdminApplicationPlaces.java
@@ -194,6 +194,5 @@ public final class WebAdminApplicationPlaces {
     public static final String DEFAULT_MAIN_SECTION_PLACE = virtualMachineMainPlace;
 
     public static final String securitySettingsMainPlace = "securitySettings"; //$NON-NLS-1$
-    public static final String auditLogMainPlace = "auditLog"; //$NON-NLS-1$
 
 }
--- frontend/webadmin/modules/webadmin/src/main/java/org/ovirt/engine/ui/webadmin/gin/PresenterModule.java
+++ frontend/webadmin/modules/webadmin/src/main/java/org/ovirt/engine/ui/webadmin/gin/PresenterModule.java
@@ -103,7 +103,6 @@ import org.ovirt.engine.ui.uicommonweb.models.vms.VmListModel;
 import org.ovirt.engine.ui.uicommonweb.models.volumes.VolumeListModel;
 import org.ovirt.engine.ui.webadmin.section.main.presenter.AboutPopupPresenterWidget;
 import org.ovirt.engine.ui.webadmin.section.main.presenter.HeaderPresenterWidget;
-import org.ovirt.engine.ui.webadmin.section.main.presenter.MainAuditLogPresenter;
 import org.ovirt.engine.ui.webadmin.section.main.presenter.MainClusterPresenter;
 import org.ovirt.engine.ui.webadmin.section.main.presenter.MainContentPresenter;
 import org.ovirt.engine.ui.webadmin.section.main.presenter.MainDataCenterPresenter;
@@ -393,7 +392,6 @@ import org.ovirt.engine.ui.webadmin.section.main.presenter.tab.virtualMachine.Su
 import org.ovirt.engine.ui.webadmin.section.main.presenter.tab.virtualMachine.VirtualMachineSubTabPanelPresenter;
 import org.ovirt.engine.ui.webadmin.section.main.view.AboutPopupView;
 import org.ovirt.engine.ui.webadmin.section.main.view.HeaderView;
-import org.ovirt.engine.ui.webadmin.section.main.view.MainAuditLogView;
 import org.ovirt.engine.ui.webadmin.section.main.view.MainClusterView;
 import org.ovirt.engine.ui.webadmin.section.main.view.MainContentView;
 import org.ovirt.engine.ui.webadmin.section.main.view.MainDataCenterView;
@@ -786,10 +784,6 @@ public class PresenterModule extends BasePresenterModule {
                 MainSecuritySettingsView.class,
                 MainSecuritySettingsPresenter.ProxyDef.class);
         bind(SecuritySettingsActionPanelPresenterWidget.class);
-        bindPresenter(MainAuditLogPresenter.class,
-                MainAuditLogPresenter.ViewDef.class,
-                MainAuditLogView.class,
-                MainAuditLogPresenter.ProxyDef.class);
         bind(AuditLogActionPanelPresenterWidget.class);
         bindPresenter(MainStoragePresenter.class,
                 MainStoragePresenter.ViewDef.class,
