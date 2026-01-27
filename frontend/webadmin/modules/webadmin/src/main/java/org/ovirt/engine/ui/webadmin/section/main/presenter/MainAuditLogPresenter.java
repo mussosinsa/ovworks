4a4739946d65c18fdca8719dc9eb7b9239f71e05
mussosinsa <62009100+mussosinsa@users.noreply.github.com>
2026-01-28 06:23:35 +0900

    Mark audit log place token as non-localized

--- frontend/webadmin/modules/webadmin/src/main/java/org/ovirt/engine/ui/webadmin/section/main/presenter/MainAuditLogPresenter.java
+++ frontend/webadmin/modules/webadmin/src/main/java/org/ovirt/engine/ui/webadmin/section/main/presenter/MainAuditLogPresenter.java
@@ -26,7 +26,7 @@ public class MainAuditLogPresenter extends AbstractMainWithDetailsPresenter<Obje
     }
 
     @ProxyCodeSplit
-    @NameToken("auditLog")
+    @NameToken("auditLog") //$NON-NLS-1$
     public interface ProxyDef extends ProxyPlace<MainAuditLogPresenter> {
     }
 
@@ -52,7 +52,7 @@ public class MainAuditLogPresenter extends AbstractMainWithDetailsPresenter<Obje
 
     @Override
     protected PlaceRequest getMainViewRequest() {
-        return PlaceRequestFactory.get("auditLog");
+        return PlaceRequestFactory.get("auditLog"); //$NON-NLS-1$
     }
 
     @Override
