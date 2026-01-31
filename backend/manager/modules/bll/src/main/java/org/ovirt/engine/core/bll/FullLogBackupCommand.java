d796d6e9dcbe7eb0fd8470117709a0b5be6bf344
mussosinsa <62009100+mussosinsa@users.noreply.github.com>
2026-01-31 22:10:59 +0900

    Treat backup script success output as success

--- backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/FullLogBackupCommand.java
+++ backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/FullLogBackupCommand.java
@@ -51,6 +51,8 @@ public class FullLogBackupCommand extends CommandBase<AuditLogBackupParameters> 
         getReturnValue().setActionReturnValue(result.output);
         if (result.exitCode == 0) {
             setSucceeded(true);
+        } else if (result.output != null && result.output.startsWith("SUCCESS")) { //$NON-NLS-1$
+            setSucceeded(true);
         } else {
             getReturnValue().getExecuteFailedMessages().add("전체 로그 백업 실패 (종료 코드: " //$NON-NLS-1$
                     + result.exitCode + ")\n" + result.output); //$NON-NLS-1$
