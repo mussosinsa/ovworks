3c1880cfbcd9b05d7312bbf044f01513ee5c868c
mussosinsa <62009100+mussosinsa@users.noreply.github.com>
2026-03-21 23:09:48 +0900

    Strengthen engine setup admin password policy

--- packaging/setup/plugins/ovirt-engine-setup/ovirt-engine/config/aaa.py
+++ packaging/setup/plugins/ovirt-engine-setup/ovirt-engine/config/aaa.py
@@ -13,6 +13,7 @@
 import gettext
 import random
 import string
+import re
 
 from otopi import plugin
 from otopi import util
@@ -41,6 +42,8 @@ def _(m):
 class Plugin(plugin.PluginBase):
     """aaa plugin."""
 
+    _MIN_ADMIN_PASSWORD_LENGTH = 10
+
     @staticmethod
     def _generatePassword():
         return ''.join([
@@ -52,6 +55,63 @@ class Plugin(plugin.PluginBase):
 
     def __init__(self, context):
         super(Plugin, self).__init__(context=context)
+
+    def _validateAdminPasswordPolicy(self, password):
+        admin_user = self.environment[
+            oenginecons.ConfigEnv.ADMIN_USER
+        ].split('@', 1)[0].lower()
+
+        if len(password) < self._MIN_ADMIN_PASSWORD_LENGTH:
+            raise RuntimeError(
+                _(
+                    'Password must be at least {length} characters long'
+                ).format(
+                    length=self._MIN_ADMIN_PASSWORD_LENGTH,
+                )
+            )
+
+        complexity_checks = (
+            (r'[a-z]', _('Password must contain a lowercase letter')),
+            (r'[A-Z]', _('Password must contain an uppercase letter')),
+            (r'[0-9]', _('Password must contain a digit')),
+            (r'[^A-Za-z0-9]', _('Password must contain a special character')),
+        )
+        for pattern, message in complexity_checks:
+            if re.search(pattern, password) is None:
+                raise RuntimeError(message)
+
+        lowered_password = password.lower()
+        if admin_user and admin_user in lowered_password:
+            raise RuntimeError(
+                _('Password must not contain the account name')
+            )
+
+        weak_words = (
+            'password',
+            'admin',
+            'ovirt',
+            'engine',
+            'welcome',
+            'qwerty',
+        )
+        for weak_word in weak_words:
+            if weak_word in lowered_password:
+                raise RuntimeError(
+                    _('Password must not contain common dictionary words')
+                )
+
+        for sequence in ('0123456789', 'abcdefghijklmnopqrstuvwxyz'):
+            for index in range(len(sequence) - 2):
+                token = sequence[index:index + 3]
+                if token in lowered_password or token[::-1] in lowered_password:
+                    raise RuntimeError(
+                        _('Password must not contain sequential characters')
+                    )
+
+        if re.search(r'(.)\1\1', password) is not None:
+            raise RuntimeError(
+                _('Password must not contain repeated characters')
+            )
 
     @plugin.event(
         stage=plugin.Stages.STAGE_BOOT,
@@ -126,6 +186,22 @@ class Plugin(plugin.PluginBase):
     def _customization(self):
         valid = False
         password = None
+        self.logger.info(
+            _(
+                'Password policy: use at least {minimum} characters '
+                '(10-12 or more recommended), combine uppercase/lowercase '
+                'letters, digits and special characters, and avoid account '
+                'names, dictionary words and sequential strings.'
+            ).format(
+                minimum=self._MIN_ADMIN_PASSWORD_LENGTH,
+            )
+        )
+        self.logger.info(
+            _(
+                'Operational policy reminder: do not reuse old passwords and '
+                'rotate them according to your security standard.'
+            )
+        )
         while not valid:
             password = self.dialog.queryString(
                 name='OVESETUP_CONFIG_ADMIN_SETUP',
@@ -144,6 +220,7 @@ class Plugin(plugin.PluginBase):
                 self.logger.warning(_('Passwords do not match'))
             else:
                 try:
+                    self._validateAdminPasswordPolicy(password)
                     if(_use_pwquality):
                         pwq = pwquality.PWQSettings()
                         pwq.read_config()
