d2fedf3149e3e3ea2467b9f513c94d5af7d57b49
mussosinsa <62009100+mussosinsa@users.noreply.github.com>
2026-04-10 16:21:01 +0900

    fix checkstyle import order in terminal IP test

--- backend/manager/modules/bll/src/test/java/org/ovirt/engine/core/bll/TerminalIpConfigUtilsTest.java
+++ backend/manager/modules/bll/src/test/java/org/ovirt/engine/core/bll/TerminalIpConfigUtilsTest.java
@@ -1,10 +1,10 @@
 package org.ovirt.engine.core.bll;
 
-import java.io.IOException;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.io.IOException;
 
 import org.junit.jupiter.api.Test;
 
