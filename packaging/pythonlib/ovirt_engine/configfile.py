7bc6d740f2ae4a71e658f3d193e9e74980bb0555
mussosinsa <62009100+mussosinsa@users.noreply.github.com>
2026-08-05 22:30:29 +0900

    Exclude AAA JDBC config from encryption allowlist

--- packaging/pythonlib/ovirt_engine/configfile.py
+++ packaging/pythonlib/ovirt_engine/configfile.py
@@ -23,7 +23,6 @@ _ENCRYPTOR_CONFIG_PATH = '/etc/ovirt-engine/encryptor/config.json'
 _ENCRYPTED_CONFIG_BASENAMES = frozenset((
     '10-setup-database.conf',
     '10-setup-dwh-database.conf',
-    'internal.properties',
 ))
 _ENCRYPTED_MAGIC = b'OVENC001'
 
